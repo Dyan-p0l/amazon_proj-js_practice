9a84189b75bfb6e0ab82573955e429f3655e915d
John Paul <johnpaulrayco1@gmail.com>
2024-07-21 17:27:41 +0800

    used localStorage to retain data after refresh

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,23 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 1
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 2
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 1
+        },
+        {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 2
+        }
+    ];
+}
+
+
+
+function saveToLocal () {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export const addToCart = (productId) => {
     let matchingItem;
@@ -23,16 +33,18 @@ export const addToCart = (productId) => {
             quantity: 1
         });
     }
-
+    saveToLocal();
 };
 
 export const removeToCart = (productId) => {
     const newCart = [];
-    
+
     cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
         newCart.push(cartItem);
     }
     cart = newCart;
     });
+
+    saveToLocal();
 }
