eff8f94d0139e2aac2dd86a8f68f2c0613684f46
John Paul <johnpaulrayco1@gmail.com>
2024-08-05 16:50:07 +0800

    added html to payment summary

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -1,3 +1,45 @@
 export function renderPaymentSummary() {
-    console.log('this is a function to render payment summary in the right section of the page');
+
+    let paymentHTML = '';
+
+    const paymentSummary = document.querySelector('.js-payment-summary');
+
+    paymentHTML += 
+    `
+        <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$42.75</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$4.99</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$47.74</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$4.77</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$52.51</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+    `;
+
+
+    paymentSummary.innerHTML = paymentHTML;
 }
