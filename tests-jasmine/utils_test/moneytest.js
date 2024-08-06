0e735d37039f79b32f0ba83f61705cefc5e2de16
John Paul <johnpaulrayco1@gmail.com>
2024-08-06 19:57:57 +0800

    added cart js test

--- tests-jasmine/utils_test/moneytest.js
+++ tests-jasmine/utils_test/moneytest.js
@@ -1,10 +1,9 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 describe('test suite: formatCurrency', () => {
     it('rounds up cent values', () => {
         expect(formatCurrency(2000.5)).toEqual('20.01');
     });
-
 
     it('works with zero', () => {
         expect(formatCurrency(0)).toEqual('0.00');
