f946e2b817a09bd803f44e342bb86304bb6bf3bb
John Paul <johnpaulrayco1@gmail.com>
2024-08-07 17:02:48 +0800

    added test for removing html of order summary

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -51,7 +51,7 @@ export function renderOrderSummary() {
                     <span class="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
--- tests-jasmine/checkout_test/ordersummaryTest.js
+++ tests-jasmine/checkout_test/ordersummaryTest.js
@@ -27,6 +27,38 @@ describe('test suite: renderOrderSummary', () => {
         renderOrderSummary();
 
         expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+    });
+
+    it('removes a product', () => {
+        document.querySelector('.js-test-container').innerHTML = `
+        <div class="js-order-summary"></div>
+        <div class="js-payment-summary"></div>
+        `;
+
+        
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 1,
+                    deliveryTimeId: '1'
+                },
+                {
+                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                    quantity: 2,
+                    deliveryTimeId: '2'
+                }
+            ]);
+        });
+
+        console.log(cart);
+
+        loadFromStorage();
+        
+        renderOrderSummary();
+
+        document.querySelector('.js-delete-link-e43638ce-6aa0-4b85-b27f-e1d07eb678c6').click();
 
     });
 });
